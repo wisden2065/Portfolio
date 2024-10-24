c7866f10eda074adb18235b77f4dee3b3a9ff15f
Nnanyereugo <nnajiwisdom1998@gmail.com>
2024-10-24 09:54:51 +0100

    Implemented toggle for dropdownmenu

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -4,9 +4,22 @@ import menu_icon from '../../Images/menu_bar.png'
 import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faXmark } from '@fortawesome/free-solid-svg-icons'; 
+import { useEffect } from 'react';
 const Navbar = () => {
   // add a state to toggle active btn
   const [active, setActive] = useState('');
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
+    let menu = document.querySelector('.drop-down-wrapper');
+    let icon = document.querySelector('.menu_icon')
+    function toggleDropdown(){
+        menu.classList.toggle('active');
+        // icon.setAttribute('data-icon', 'xMark');
+        // console.log(icon.attributes['data-icon'].value);
+        setIsMenuOpen(p=> p = !p)
+
+
+    }
 
     return (
           <nav>
@@ -18,7 +31,18 @@ const Navbar = () => {
                   <li className={active === 'Work'? 'active': ''}  onClick={()=>{setActive('Work')}}>Work</li>
                   <li className={active === 'Contact'? 'active': ''}  onClick={()=>{setActive('Contact')}}>Contact</li>
               </ul>   
-              <FontAwesomeIcon className='menu_icon' icon={faBars} /> 
+              <FontAwesomeIcon className='menu_icon' icon={isMenuOpen == true? faXmark : faBars} onClick={toggleDropdown} />
+              <div className="drop-down-wrapper">
+                <div className="drop-down">
+                    <ul>
+                        <li><a href="#">About</a></li>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Work</a></li>
+                        <li><a href="#">Skills</a></li>
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </div>
+              </div> 
           </nav>
     )
 }
