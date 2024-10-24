0c7e071c243e9b9fe1171f44abc035e25d5cc8eb
Nnanyereugo <nnajiwisdom1998@gmail.com>
2024-10-24 10:43:16 +0100

    conditonally rendered dropdown menu to fix dropdown not working

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -5,19 +5,25 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons'; 
-import { useEffect } from 'react';
+
+
+
 const Navbar = () => {
   // add a state to toggle active btn
   const [active, setActive] = useState('');
-  const [isMenuOpen, setIsMenuOpen] = useState(false)
-    let menu = document.querySelector('.drop-down-wrapper');
-    let icon = document.querySelector('.menu_icon')
+//   state to check if the icon is clicked or not
+    const [iconClicked, setIconClicked] = useState(false)
+//   state to check if dropdown is open or not
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    // let menu = document.querySelector('.drop-down-wrapper');
+    // let icon = document.querySelector('.menu_icon');
     function toggleDropdown(){
-        menu.classList.toggle('active');
+        // menu.classList.toggle('active');
         // icon.setAttribute('data-icon', 'xMark');
         // console.log(icon.attributes['data-icon'].value);
         setIsMenuOpen(p=> p = !p)
-
+        setIconClicked(!iconClicked)
 
     }
 
@@ -32,17 +38,32 @@ const Navbar = () => {
                   <li className={active === 'Contact'? 'active': ''}  onClick={()=>{setActive('Contact')}}>Contact</li>
               </ul>   
               <FontAwesomeIcon className='menu_icon' icon={isMenuOpen == true? faXmark : faBars} onClick={toggleDropdown} />
-              <div className="drop-down-wrapper">
-                <div className="drop-down">
-                    <ul>
-                        <li><a href="#">About</a></li>
-                        <li><a href="#">Home</a></li>
-                        <li><a href="#">Work</a></li>
-                        <li><a href="#">Skills</a></li>
-                        <li><a href="#">Contact</a></li>
-                    </ul>
-                </div>
-              </div> 
+             {/* condtionally render dropdown if icon clicked is true */}
+             {iconClicked === true?
+                 <div className="drop-down-wrapper active">
+                    <div className="drop-down">
+                        <ul>
+                            <li><a href="#">About</a></li>
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Work</a></li>
+                            <li><a href="#">Skills</a></li>
+                            <li><a href="#">Contact</a></li>
+                        </ul>
+                    </div>
+               </div>
+               :
+               <div className="drop-down-wrapper">
+               <div className="drop-down">
+                   <ul>
+                       <li><a href="#">About</a></li>
+                       <li><a href="#">Home</a></li>
+                       <li><a href="#">Work</a></li>
+                       <li><a href="#">Skills</a></li>
+                       <li><a href="#">Contact</a></li>
+                   </ul>
+               </div>
+             </div> 
+            }
           </nav>
     )
 }
