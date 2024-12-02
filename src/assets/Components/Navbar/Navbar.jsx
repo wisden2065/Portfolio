62517edb9e59a4ce53614bdbb5ed63285a8c635c
Nnanyereugo <nnajiwisdom1998@gmail.com>
2024-12-02 12:04:38 +0100

    Implemented light/darkmode icon and blog section at navabar

--- src/assets/Components/Navbar/Navbar.jsx
+++ src/assets/Components/Navbar/Navbar.jsx
@@ -5,6 +5,9 @@ import { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { faXmark } from '@fortawesome/free-solid-svg-icons'; 
+import { faMoon } from '@fortawesome/free-solid-svg-icons';
+// import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faSun } from '@fortawesome/free-solid-svg-icons';
 
 
 const Navbar = () => {
@@ -14,6 +17,11 @@ const Navbar = () => {
     const [iconClicked, setIconClicked] = useState(false)
 //   state to check if dropdown is open or not
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+//   light/dark mode state
+    const [isLight, setLight] = useState(true);
+    function toggleLight(){
+        setLight(!isLight)
+    }
 
     // let menu = document.querySelector('.drop-down-wrapper');
     // let icon = document.querySelector('.menu_icon');
@@ -33,10 +41,18 @@ const Navbar = () => {
                   <li className={active === 'About'? 'active': ''}  onClick={()=>{setActive('About')}}><a href='#about-section'>About</a></li>
                   <li className={active === ''|| active ==='Home'? 'active': ''}   onClick={()=>{setActive('Home')}}><a href='#home-section'>Home</a></li>
                   <li className={active === 'Skills'? 'active': ''}  onClick={()=>{setActive('Skills')}}><a href='#skills-section'>Skills</a></li>
+                  <li className={active === 'Blog'? 'active': ''}  onClick={()=>{setActive('Blog')}}><a href='#blog-section'>Blog</a></li>
                   <li className={active === 'Work'? 'active': ''}  onClick={()=>{setActive('Work')}}><a href='#work-section'>Work</a></li>
                   <li className={active === 'Contact'? 'active': ''}  onClick={()=>{setActive('Contact')}}><a href='#contact-section'>Contact</a></li>
               </ul>   
-              <FontAwesomeIcon className='menu_icon' icon={isMenuOpen == true? faXmark : faBars} onClick={toggleDropdown} />
+             <div className='icon-cont'>
+                <div className='icon'>
+                    <FontAwesomeIcon icon={isLight? faMoon : faSun} className='i' onClick={toggleLight} />
+                </div>
+                <div className='icon'>
+                    <FontAwesomeIcon className='i' icon={isMenuOpen == true? faXmark : faBars} onClick={toggleDropdown} />
+                </div>
+             </div>
              {/* condtionally render dropdown if icon clicked is true */}
              {iconClicked === true?
                  <div className="drop-down-wrapper active">
@@ -46,6 +62,7 @@ const Navbar = () => {
                             <li><a href="#home-section">Home</a></li>
                             <li><a href="#work-section">Work</a></li>
                             <li><a href="#skills-section">Skills</a></li>
+                            <li><a href="#skills-section">Blog</a></li>
                             <li><a href="#contact-section">Contact</a></li>
                         </ul>
                     </div>
@@ -58,6 +75,7 @@ const Navbar = () => {
                        <li><a href="#home-section">Home</a></li>
                        <li><a href="#work-section">Work</a></li>
                        <li><a href="#skills-section">Skills</a></li>
+                       <li><a href="#skills-section">Blog</a></li>
                        <li><a href="#contact-section">Contact</a></li>
                    </ul>
                </div>
