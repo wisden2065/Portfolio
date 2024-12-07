a81921cc667386f6fd9e322a938001038f4f548e
Nnanyereugo <nnajiwisdom1998@gmail.com>
2024-12-07 12:50:58 +0100

    implemented map function to show all projects

--- src/assets/Components/Projects/Project.jsx
+++ src/assets/Components/Projects/Project.jsx
@@ -2,35 +2,36 @@ import React from 'react'
 import './Projects.css'
 import proj1 from '../../Images/proj1.jpg'
 const Project = () => {
+    let projects = [
+        {
+            "Project Title": "Munchino E-commerce",
+            "Project Thumbnail": proj1,
+            "Project Description": "This is a mini e-commerce store for food delivery",
+            "Project Stack": ['HTML', 'CSS', 'JAvascript', 'PHP']
+        },
+        {
+            "Project Title": "Konga Clone",
+            "Project Thumbnail": proj1,
+            "Project Description": "This is a mini clone for konga, a successful e-commerce store in Nigeria",
+            "Project Stack": ['HTML', 'CSS', 'JAvascript', 'NodeJs', 'ExpressJs']
+        }
+    ]
   return (
     <section className='projects_' id='work-section'>
        <h3 className='skills_title'>Projects</h3>
        <div className="projectWrapper">
             <div id='projectsCont'>
-                <div className="proj">
-                    <div className="overlay"></div>
-                    <img src={proj1} alt="" />
-                        <div className='btnCont'>
-                        <a href="">See Project Live</a>
-                        <a href="">View Source code</a>
-                    </div>
-                </div>
-                <div className="proj">
-                    <div className="overlay"></div>
-                    <img src={proj1} alt="" />
-                    <div className='btnCont'>
-                        <a href="https://kongaui-clone-bq0jw5src-nnanyereugos-projects.vercel.app/">See Project Live</a>
-                        <a href="https://github.com/wisden2065/kongaclone.git">View Source code</a>
-                    </div>
-                </div>
-                <div className="proj">
-                    <div className="overlay"></div>
-                    <img src={proj1} alt="" />
-                    <div className='btnCont'>
-                        <a href="">See Project Live</a>
-                        <a href="">View Source code</a>
-                    </div>
-                </div>
+              {projects.map(project=>{
+                return  <div className="proj">
+                            <div className="overlay">
+                                <h4>{project['Project Title']}</h4>
+                                <p>{project['Project Description']}</p>
+                                <p>Built with: {project['Project Stack'].map(p=> p.split(" "))}</p>
+                            </div>
+                            <img src={proj1} alt="" />
+                           
+                        </div>
+              })}
             </div>
 
        </div>
